3dd73d21ad8b7a2f4d8ad6c63c349dfda2310262
静文（jingwen） <jingwen@xiaoshijie.com>
2018-12-03 17:18:03 +0800

    feature

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+let path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname,'src/index.js'),
+  output:{
+    path: path.resolve(__dirname,'build'),
+    filename: '[name].js'
+  },
+  module:{
+    rules:[
+      {test: /\.js?$/, use: 'babel-loader',},
+    ],
+  }
+}
