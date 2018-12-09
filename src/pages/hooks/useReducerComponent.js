9921f8f3ecc6f4d9ebae0c83e96b9066bc179dc5
静文（jingwen） <jingwen@xiaoshijie.com>
2018-12-09 15:53:07 +0800

    feature

--- src/pages/hooks/useReducerComponent.js
+++ src/pages/hooks/useReducerComponent.js
@@ -1,12 +1,33 @@
 import React,{useReducer} from 'react';
 import Fieldset from '@Component/Fieldset';
 
+function reducer(state,action){
+  switch(action){
+    case 'reset':
+      return 0;
+      break;
+    case 'increment':
+      return state+1;
+      break;
+    case 'decrement':
+      return state-1;
+      break;
+    default:
+      return state;
+  };
+};
+
 function useReduceComponent(){
+  const [state,dispatch] = useReducer(reducer,0);
+
   return (
     <Fieldset title='useReduce'>
-      useReduce
+      <p>{state}</p>
+      <input type='button' value='++' onClick={()=>dispatch('increment')}/>
+      <input type='button' value='--' onClick={()=>dispatch('decrement')}/>
+      <input type='button' value='reset' onClick={()=>dispatch('reset')}/>
     </Fieldset>
-  )
+  );
 };
 
 export default useReduceComponent;
