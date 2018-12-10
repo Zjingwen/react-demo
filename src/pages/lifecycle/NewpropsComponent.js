bd5d7698a5d8a4139479b87ccc99c7b82ea9c55c
静文（jingwen） <jingwen@xiaoshijie.com>
2018-12-10 15:41:25 +0800

    * change new props

--- src/pages/lifecycle/NewpropsComponent.js
+++ src/pages/lifecycle/NewpropsComponent.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Fieldset from '@Component/Fieldset';
 
+class Flow extends React.Component{
+  state = {};
+
+  static getDerivedStateFromProps(){
+    console.group('触发传递新的props');
+    console.log('getDevivedStateFromProps');
+    return null;
+  };
+
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate');
+    return true;
+  };
+
+  getSnapshotBeforeUpdate(){
+    console.log('getSnapshotBeforeUpdate');
+    return null;
+  };
+
+  componentDidUpdate(){
+    console.log('componentDidUpdate');
+    console.groupEnd();
+  };
+
+  render(){
+    console.log('render');
+    return(
+      <React.Fragment>
+        <p>{this.props.title}</p>
+        <p>
+          static getDevivedStateFromPorps <br/>
+          | <br/>
+          shouldComponentUpdate <br/>
+          | <br/>
+          getSnapshotBeforeUpdate <br/>
+          | <br/>
+          componentDidUpdate
+        </p>
+      </React.Fragment>
+    )
+  }
+};
+
 function NewpropsComponent(){
+  const [time,setTime] = useState(new Date().toString());
+
   return (
     <Fieldset title='传递新的props'>
-      传递新的props
+      <input type='button' value='触发传递新的props' onClick={()=>setTime(new Date().toString())}/>
+      <Flow title={time}/>
     </Fieldset>
   )
 };
