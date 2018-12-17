aaca3a5a5abf18bc7ec00087de41860abce4d1a7
静文（jingwen） <jingwen@xiaoshijie.com>
2018-12-17 13:40:06 +0800

    * feature: 添加memo的demo

--- src/pages/memo/index.js
+++ src/pages/memo/index.js
@@ -1,16 +1,29 @@
 import React, {
-  Component,
+  useState,
 } from 'react';
 import Fieldset from '@Component/Fieldset';
 
-class Memo extends Component {
-  render() {
-    return (
-      <Fieldset title='memo'>
-        memo
-      </Fieldset>
-    );
+const MemoComponent = React.memo((props)=>{
+  return (
+    <div>{props.time}</div>
+  );
+}, (prevProps, nextProps)=>{
+  if (prevProps.time !== nextProps.time) {
+    return null;
   }
+  return false;
+});
+
+function Memo() {
+  const [time, setTime] = useState(new Date().toString());
+
+  return (
+    <Fieldset title='memo'>
+      memo
+      <input type='button' value='更新时间' onClick={()=>setTime(new Date().toString())} />
+      <MemoComponent time={time} />
+    </Fieldset>
+  );
 };
 
 export default Memo;
